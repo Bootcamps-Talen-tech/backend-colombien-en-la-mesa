46f092abac135415a81bbcc4e510483df41d33fe
Moi~Rouhs <carsrouhs@gmail.com>
2024-07-06 15:40:50 -0500

    remove config

--- database/connection.js
+++ database/connection.js
@@ -2,13 +2,13 @@
 import { connect } from "mongoose";
 import 'dotenv/config'
 
-const db_url = process.env.DB_URL + '?authSource=shrimp';
+const db_url = process.env.DB_URL ;
 
 console.log('url conección:', db_url);
 const connection = async()=>{
   try {
     await connect(db_url);
-    console.log("Conectado correctamente a la BD: bd_recettes");
+    console.log("Conectado correctamente a la BD");
   } catch (error) {
     console.log(error);
     throw new error("¡No se ha podido conectar a la base de datos!");
