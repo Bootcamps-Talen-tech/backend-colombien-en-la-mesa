d2c319db9b21e5d44c7c180dda78750424a162db
Moi~Rouhs <carsrouhs@gmail.com>
2024-07-07 16:48:24 -0500

    clear code

--- services/recipe.js
+++ services/recipe.js
@@ -2,6 +2,5 @@ import Recipes from "../models/recipes.js";
 
 export const searchRecipeById = async (recipeId)=>{
   const recipe = await Recipes.findById(recipeId)
-  console.log('recipe services:', recipe)
   return recipe;
 }
